9e1f9d2a9e28dc9bb576d9e541e863de2190860b
Mostakim Niloy <niloy19@Niloys-Mac.local>
2025-05-05 20:28:07 +0600

    setup done2

---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import {
+    createBrowserRouter,
+  } from "react-router";
+  
+  export const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <div>Hello World</div>,
+    },
+  ]);
